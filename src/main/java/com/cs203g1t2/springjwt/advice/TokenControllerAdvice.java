3b4b7b2af2a9be64c29d3de3f3cfef1e94f96068
Ryan Hong <ryanhong1126@gmail.com>
2022-10-06 18:01:57 +0800

    refresh token

---
+++ src/main/java/com/cs203g1t2/springjwt/advice/TokenControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.cs203g1t2.springjwt.advice;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.cs203g1t2.springjwt.exceptions.TokenRefreshException;
+
+@RestControllerAdvice
+public class TokenControllerAdvice {
+
+  @ExceptionHandler(value = TokenRefreshException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public ErrorMessage handleTokenRefreshException(TokenRefreshException ex, WebRequest request) {
+    return new ErrorMessage(
+        HttpStatus.FORBIDDEN.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+  }
+}
